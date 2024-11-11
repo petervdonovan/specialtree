96fd075679493b3fff590ac2444454ed510e6a8b
Peter Donovan <peter@xronos.com>
2024-11-10 20:51:32 -0800

    Demonstrate working serde feature

--- langserde/src/lib.rs
+++ langserde/src/lib.rs
@@ -1,8 +1,4 @@
-use langspec::{
-    flat::LangSpecFlat,
-    humanreadable::LangSpecHuman,
-    langspec::{LangSpec, TerminalLangSpec as _},
-};
+use langspec::{flat::LangSpecFlat, humanreadable::LangSpecHuman, langspec::TerminalLangSpec as _};
 use langspec_gen_util::LangSpecGen;
 
 pub struct BasePaths {
@@ -10,31 +6,40 @@ pub struct BasePaths {
     pub extension_of: syn::Path,
 }
 
-pub fn gen<L: LangSpec>(
+pub fn gen(
     BasePaths {
         refbased_data_structure,
         extension_of,
     }: &BasePaths,
-    lg: &LangSpecGen<L>,
-) -> syn::ItemFn {
+    ty_camel_ident: &syn::Ident,
+) -> (syn::ItemFn, syn::ItemFn) {
     let byline = langspec_gen_util::byline!();
-    let camel_ident = lg.common_gen_datas().camel_ident;
-    syn::parse_quote! {
+    let parse = syn::parse_quote! {
         #byline
-        fn parse<LImpl: #extension_of::LImpl>(input: &str) -> Result<(LImpl, #extension_of::Any<LImpl>), ()> {
-            #(
-                let refbased: Result<#refbased_data_structure::#camel_ident, _> = serde_yml::from_str(input);
-                if let Ok(refbased) = refbased {
-                    use #extension_of::reference::#camel_ident;
-                    let l = core::default::Default::default();
-                    let mut lo = core::default::Default::default();
-                    let ret = refbased.convert(&l, &mut lo);
-                    return Ok((lo, #extension_of::Any::#camel_ident(ret)));
-                }
-            )*
-            Err(())
+        fn parse<LImpl: #extension_of::LImpl, #ty_camel_ident: #extension_of::owned::#ty_camel_ident<LImpl = LImpl>>(input: &str) -> Result<(LImpl, #ty_camel_ident), ()> {
+            let refbased: #refbased_data_structure::#ty_camel_ident = serde_json::from_str(input).map_err(|e| println!("Parse error: {e}"))?;
+            use #extension_of::reference::#ty_camel_ident;
+            let l = core::default::Default::default();
+            let mut lo = core::default::Default::default();
+            let ret = refbased.convert(&l, &mut lo);
+            Ok((lo, ret))
         }
-    }
+    };
+    let unparse = syn::parse_quote! {
+        #byline
+        fn unparse<
+            LImpl: #extension_of::LImpl,
+            #ty_camel_ident: #extension_of::owned::#ty_camel_ident<LImpl = LImpl>
+        >(
+            lo: &LImpl, own: #ty_camel_ident
+        ) -> Result<String, ()> {
+            use #extension_of::reference::#ty_camel_ident;
+            let mut l = core::default::Default::default();
+            let refbased: #refbased_data_structure::#ty_camel_ident = own.get_ref(lo).convert(lo, &mut l);
+            serde_json::to_string_pretty(&refbased).map_err(|_| ())
+        }
+    };
+    (parse, unparse)
 }
 
 pub fn formatted(lsh: &LangSpecHuman) -> String {
@@ -45,13 +50,12 @@ pub fn formatted(lsh: &LangSpecHuman) -> String {
     };
     let idxbased_data_structure_path: syn::Path =
         syn::parse_quote!(crate::data_structure::idxbased);
-    // let idxbased_extensionof_path: syn::Path = syn::parse_quote!(crate::extension_of::idxbased);
     let lsfg = LangSpecGen {
         bak: &lsf,
         sort2rs_type: |_, _| syn::parse_quote!(syn::Type),
         type_base_path: syn::parse_quote!(crate::types),
     };
-    let m = gen(&bps, &lsfg);
+    let (parse, unparse) = gen(&bps, &syn::parse_quote! {Nat});
     let refbased_extension_of = refbased_extensionof_gen::gen(
         &refbased_extensionof_gen::BasePaths {
             extension_of: bps.extension_of.clone(),
@@ -75,14 +79,30 @@ pub fn formatted(lsh: &LangSpecHuman) -> String {
     let test: syn::ItemFn = syn::parse_quote! {
         #[test]
         fn round_trip() {
-            let input = std::fs::read_to_string("tests/fib.yml").unwrap();
-            let (l, lo) = parse::<#idxbased_data_structure_path::#ls_camel_name>(&input).unwrap();
-            let expected = expect_test::expect![[]];
-            expected.assert_debug_eq(&lo);
+            let input = std::fs::read_to_string("tests/fib.json").unwrap();
+            let (lo, nat) = parse::<#idxbased_data_structure_path::#ls_camel_name, #idxbased_data_structure_path::Nat>(&input).unwrap();
+            let unparsed = unparse(&lo, nat).unwrap();
+            let expected = expect_test::expect![[r#"
+{
+  "Plus": [
+    {
+      "F": {
+        "NatLit": 50
+      }
+    },
+    {
+      "F": {
+        "NatLit": 51
+      }
+    }
+  ]
+}"#]];
+            expected.assert_eq(&unparsed);
         }
     };
     prettyplease::unparse(&syn::parse_quote!(
-        #m
+        #parse
+        #unparse
         #refbased_extension_of
         #idxbased_extension_of
         #extension_of
