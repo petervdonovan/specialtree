316a12ce8ecc0279157b7df158ab0abf8e85b0aa
Peter Donovan <peter@xronos.com>
2025-04-17 18:07:15 -0700

    syn_insert_use

---
+++ syn-insert-use/src/lib.rs
@@ -0,0 +1,399 @@
+use std::collections::HashSet;
+
+use syn::{visit::Visit as _, visit_mut::VisitMut};
+
+pub fn insert_use(f: syn::File) -> syn::File {
+    let mut pv = PathVisitor::<Recursive>::new();
+    pv.visit_file(&f);
+    let pv = pv.sorted();
+    let (_, cp) = use_declarations(pv);
+    let mut tersified_file = f.clone();
+    InsertUseInModuleAsNeeded.visit_file_mut(&mut tersified_file);
+    TersifyingPathsVisitor { collisions: cp }.visit_file_mut(&mut tersified_file);
+    let (top_level_use, _) = {
+        let mut pv = PathVisitor::<NonRecursive>::new();
+        pv.visit_file(&f);
+        let pv = pv.sorted();
+        use_declarations(pv)
+    };
+    for use_decl in top_level_use {
+        tersified_file.items.insert(0, use_decl.to_syn().into());
+    }
+    tersified_file
+}
+
+struct InsertUseInModuleAsNeeded;
+
+impl VisitMut for InsertUseInModuleAsNeeded {
+    fn visit_item_mod_mut(&mut self, item: &mut syn::ItemMod) {
+        if let Some((_, items)) = &mut item.content {
+            let mut pv = PathVisitor::<NonRecursive>::new();
+            for item in items.iter() {
+                pv.visit_item(item);
+            }
+            let pv = pv.sorted();
+            for use_decl in use_declarations(pv).0 {
+                items.insert(0, use_decl.to_syn().into());
+            }
+        }
+        syn::visit_mut::visit_item_mod_mut(self, item);
+    }
+}
+struct PathVisitorSorted {
+    crate_paths: Vec<Vec<syn::Ident>>,
+    external_paths: Vec<Vec<syn::Ident>>,
+}
+struct Recursive;
+struct NonRecursive;
+struct PathVisitor<RecurseMod> {
+    crate_paths: std::collections::HashSet<Vec<syn::Ident>>,
+    external_paths: std::collections::HashSet<Vec<syn::Ident>>,
+    _recurse_mod: std::marker::PhantomData<RecurseMod>,
+}
+
+impl<T> PathVisitor<T> {
+    fn new() -> Self {
+        Self {
+            crate_paths: std::collections::HashSet::new(),
+            external_paths: std::collections::HashSet::new(),
+            _recurse_mod: std::marker::PhantomData,
+        }
+    }
+    fn sorted(self) -> PathVisitorSorted {
+        let mut crate_paths = self.crate_paths.into_iter().collect::<Vec<_>>();
+        crate_paths.sort_by(|a, b| {
+            let a = a.iter().map(|x| x.to_string()).collect::<String>();
+            let b = b.iter().map(|x| x.to_string()).collect::<String>();
+            a.cmp(&b)
+        });
+        let mut external_paths = self.external_paths.into_iter().collect::<Vec<_>>();
+        external_paths.sort_by(|a, b| {
+            let a = a.iter().map(|x| x.to_string()).collect::<String>();
+            let b = b.iter().map(|x| x.to_string()).collect::<String>();
+            a.cmp(&b)
+        });
+        PathVisitorSorted {
+            crate_paths,
+            external_paths,
+        }
+    }
+}
+
+fn to_segments(path: &[syn::PathSegment]) -> Vec<syn::Ident> {
+    path.iter().map(|segment| segment.ident.clone()).collect()
+}
+
+fn path_visitor_visit_type<T>(pv: &mut PathVisitor<T>, item: &syn::Type)
+where
+    PathVisitor<T>: for<'ast> syn::visit::Visit<'ast>,
+{
+    match item {
+        syn::Type::Path(type_path) => {
+            let (head, tail) = split_path(&type_path.path.segments);
+            let path = to_segments(&head);
+            if !path.is_empty() {
+                // self.paths.insert(path);
+                if is_crate(&path) {
+                    pv.crate_paths.insert(path);
+                } else {
+                    pv.external_paths.insert(to_segments(
+                        head.iter()
+                            .cloned()
+                            .chain(std::iter::once(tail.first().unwrap()).cloned())
+                            .collect::<Vec<_>>()
+                            .as_slice(),
+                    ));
+                }
+            }
+        }
+        _ => {
+            // do nothing
+        }
+    }
+    syn::visit::visit_type(pv, item);
+}
+
+impl<'ast> syn::visit::Visit<'ast> for PathVisitor<Recursive> {
+    fn visit_type(&mut self, item: &'ast syn::Type) {
+        path_visitor_visit_type(self, item);
+    }
+}
+
+impl<'ast> syn::visit::Visit<'ast> for PathVisitor<NonRecursive> {
+    fn visit_type(&mut self, item: &'ast syn::Type) {
+        path_visitor_visit_type(self, item);
+    }
+    fn visit_item_mod(&mut self, _: &'ast syn::ItemMod) {
+        // don't visit the mod content
+    }
+    fn visit_file(&mut self, item: &'ast syn::File) {
+        for item in item
+            .items
+            .iter()
+            .filter(|item| !matches!(item, syn::Item::Use(_) | syn::Item::Mod(_)))
+        {
+            self.visit_item(item);
+        }
+    }
+}
+
+fn split_path(
+    path: &syn::punctuated::Punctuated<syn::PathSegment, syn::token::PathSep>,
+) -> (Vec<syn::PathSegment>, Vec<syn::PathSegment>) {
+    for idx in 0..path.len() {
+        if path[idx]
+            .ident
+            .to_string()
+            .chars()
+            .next()
+            .unwrap()
+            .is_ascii_uppercase()
+        {
+            return (
+                path.iter().take(idx).cloned().collect(),
+                path.iter().skip(idx).cloned().collect(),
+            );
+        }
+    }
+    (
+        path.iter().take(path.len() - 1).cloned().collect(),
+        path.iter().skip(path.len() - 1).cloned().collect(),
+    )
+}
+
+fn terse_prefix(path: &[syn::Ident]) -> syn::Ident {
+    if path.len() == 1 {
+        return path[0].clone();
+    }
+    syn::Ident::new(
+        &path
+            .iter()
+            .skip(1)
+            .flat_map(|ident| {
+                ident
+                    .to_string()
+                    .split("_")
+                    .map(|word| word.chars().next().unwrap())
+                    .collect::<Vec<_>>()
+            })
+            .fold(String::new(), |mut acc, x| {
+                acc.push(x);
+                acc
+            }),
+        proc_macro2::Span::call_site(),
+    )
+}
+
+fn is_crate(path: &[syn::Ident]) -> bool {
+    path.first().unwrap() == "crate"
+}
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct UseDeclaration {
+    path: Vec<syn::Ident>,
+    alias: Option<syn::Ident>,
+}
+
+impl UseDeclaration {
+    fn to_syn(&self) -> syn::ItemUse {
+        let path_prefix = self.path.iter().take(self.path.len() - 1);
+        let path_last = self.path.last().unwrap();
+        match &self.alias {
+            None => syn::parse_quote! {
+                use #(#path_prefix::)*#path_last;
+            },
+            Some(alias) => syn::parse_quote! {
+                use #(#path_prefix::)*#path_last as #alias;
+            },
+        }
+    }
+}
+
+impl std::fmt::Display for UseDeclaration {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = self.to_syn();
+        quote::quote! {#s}.fmt(f)
+    }
+}
+#[derive(Debug, Clone)]
+struct CollidingPaths {
+    collisions: Vec<Vec<syn::Ident>>,
+}
+
+fn use_declarations(paths: PathVisitorSorted) -> (Vec<UseDeclaration>, CollidingPaths) {
+    let uses = paths
+        .crate_paths
+        .iter()
+        .filter(|path| path.len() > 1)
+        .map(|path| {
+            let path_prefix = path.to_vec();
+            let tersed = terse_prefix(path);
+            (
+                UseDeclaration {
+                    path: path_prefix.clone(),
+                    alias: Some(tersed.clone()),
+                },
+                tersed.clone(),
+                path.clone(),
+            )
+        })
+        .chain(
+            paths
+                .external_paths
+                .iter()
+                .filter(|path| path.len() > 1)
+                .map(|path| {
+                    (
+                        UseDeclaration {
+                            path: path.clone(),
+                            alias: None,
+                        },
+                        path.last().unwrap().clone(),
+                        path.clone(),
+                    )
+                }),
+        )
+        .collect::<Vec<_>>();
+    let mut unique_uses = std::collections::HashMap::new();
+    let mut cp = CollidingPaths { collisions: vec![] };
+    let mut deduplicated = HashSet::new();
+    for use_decl in &uses {
+        if unique_uses.contains_key(&use_decl.1) && unique_uses[&use_decl.1] != use_decl.2 {
+            cp.collisions.push(use_decl.2.clone());
+            continue;
+        }
+        unique_uses.insert(use_decl.1.clone(), use_decl.2.clone());
+        deduplicated.insert(use_decl.0.clone());
+    }
+    (deduplicated.into_iter().collect(), cp)
+}
+
+struct TersifyingPathsVisitor {
+    collisions: CollidingPaths,
+}
+
+// impl<'ast> syn::visit::Visit<'ast> for TersifyingPathsVisitor {
+//     fn visit_type(&mut self, item: &'ast syn::Type) {
+//         match item {
+//             syn::Type::Path(type_path) => {
+//                 let segments = to_segments(&type_path.path);
+//                 let new_path = if is_crate(&segments) {
+//                     typ_path.path = syn::parse_quote! {
+//                         #(#segments)*
+//                     }
+//                 } else {
+//                     segments
+//                         .iter()
+//                         .map(|segment| segment.ident.clone())
+//                         .collect()
+//                 };
+//             }
+//             _ => {
+//                 // do nothing
+//             }
+//         }
+//         syn::visit::visit_type(self, item);
+//     }
+// }
+impl VisitMut for TersifyingPathsVisitor {
+    fn visit_type_mut(&mut self, item: &mut syn::Type) {
+        match item {
+            syn::Type::Path(type_path) => {
+                if type_path.qself.is_some() {
+                    return;
+                }
+                let (head, tail) = split_path(&type_path.path.segments);
+                let segments = to_segments(&head);
+                if self
+                    .collisions
+                    .collisions
+                    .iter()
+                    .any(|path| path == &segments)
+                    || head.is_empty()
+                {
+                    syn::visit_mut::visit_type_mut(self, item);
+                    return;
+                }
+                let new_path: Vec<syn::Path> = if is_crate(&segments) {
+                    let terse = terse_prefix(&segments);
+                    vec![syn::parse_quote! {#terse}]
+                } else {
+                    vec![]
+                };
+                type_path.path = syn::parse_quote! {
+                    #(#new_path::)*#(#tail)::*
+                };
+            }
+            _ => {
+                // do nothing
+            }
+        }
+        syn::visit_mut::visit_type_mut(self, item);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use syn::parse_quote;
+
+    #[test]
+    fn test_insert_use() {
+        let file: syn::File = parse_quote! {
+            fn main() {
+                let x: std::collections::HashSet<i32> = vec![1, 2, 3].into_iter().collect();
+                let x: std::incarnations::HashSet<i32> = vec![1, 2, 3].into_iter().collect();
+            }
+            pub struct Bak(
+                pub EitherHeapBak<
+                    crate::Heap,
+                    external1::Pair<
+                        crate::c_d_s::Heap,
+                        crate::c_d_s::LeftOperand,
+                        external3::segment::Maybe<crate::c_d_s::Heap, external2::ParseMetadata<crate::c_d_s::Heap>>,
+                    >,
+                    external3::segment::ParseError<crate::c_d_s::Heap>,
+                >,
+            );
+            mod foo {
+                mod bar {
+                    fn run() {
+                        let x: std::collections::HashSet<i32> = vec![1, 2, 3].into_iter().collect();
+                        let y: std::incarnations::HashSet<i32> = vec![1, 2, 3].into_iter().collect();
+                        let z: crate::foo::RunFoo = Run::new();
+                    }
+                }
+                fn bar() {
+                    let y: std::collections::HashMap<i32, i32> = vec![(1, 1), (2, 1), (3, 1)].into_iter().collect();
+                    let z: crate::foo::Bar = Bar::new();
+                    let w: crate::faa::Bar::Bar2 = Bar2::new();
+                    let w2: external::Bar::Bar2 = Bar2::new();
+                    let z: crate::foo::bar::Run = Run::new();
+                }
+                pub struct Bak(
+                    pub EitherHeapBak<
+                        crate::Heap,
+                        external1::Pair<
+                            crate::c_d_s::Heap,
+                            crate::c_d_s::LeftOperand,
+                            external3::segment::Maybe<crate::c_d_s::Heap, external2::ParseMetadata<crate::c_d_s::Heap>>,
+                        >,
+                        external3::segment::ParseError<crate::c_d_s::Heap>,
+                    >,
+                );
+            }
+        };
+        // let mut pv = PathVisitor::new();
+        // pv.visit_file(&file);
+        // dbg!(&pv.paths.len());
+        // for path in &pv.paths {
+        //     println!("{}", quote::quote! {#(#path),*});
+        // }
+        // let use_declarations = use_declarations(&pv.paths.iter().cloned().collect::<Vec<_>>());
+        // for use_decl in &use_declarations.0 {
+        //     println!("use decl = {}", use_decl);
+        // }
+        let tersified_file = insert_use(file.clone());
+        let tersified_file = prettyplease::unparse(&tersified_file);
+        println!("Tersified file = {}", tersified_file);
+    }
+}
