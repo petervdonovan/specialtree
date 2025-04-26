d5e73720e33c02837b31b403be1949ed029c10ff
Peter Donovan <peter@xronos.com>
2025-04-26 12:32:04 -0700

    Use fn pointers
    These will probably be optimized away by LLVM in practice judging from
    some tests using Godbolt

--- term/src/fnlut.rs
+++ term/src/fnlut.rs
@@ -13,26 +13,29 @@ pub trait HasFn<ArgsType, RetType> {
 // }
 
 macro_rules! impl_fn_lut {
-    (witness_name $name:ident ; trait $trai:tt ; fn_name $fn_name:ident ; fn_args $($fn_args:ty),* ; fn_ret_type $fn_ret_type:ty ; types $($typ_snake_ident:ident / $typ_camel_ident:ident = $typ:ty),*) => {
-        pub struct $name<
+    (witness_name $name:ident ; trait $trai:tt ; fn_name $fn_name:ident ; fn_args $($fn_args:ty),* ; fn_ret_type $fn_ret_type:ty ; types $($typ_snake_ident:ident = $typ:ty),*) => {
+        pub struct $name {
             $(
-                $typ_camel_ident,
+                $typ_snake_ident: fn_types::$typ_snake_ident::Ty,
             )*
-        > {
+        }
+        macro_rules! ctx_dependent_fn_type {
+            () => {
+                fn($($fn_args),*) -> $fn_ret_type
+            };
+        }
+        pub mod fn_types {
             $(
-                $typ_snake_ident: $typ_camel_ident,
+                pub mod $typ_snake_ident {
+                    use super::super::*;
+                    type This = $typ;
+                    pub type Ty = ctx_dependent_fn_type!();
+                }
             )*
         }
         macro_rules! impl_for_one {
-            ($typ2:ty, $typ_camel_ident2:ident, $typ_snake_ident2:ident) => {
-                impl<
-                    $(
-                        $typ_camel_ident,
-                    )*
-                > $crate::fnlut::HasFn<($($fn_args,)*), $fn_ret_type> for $name<$(
-                    $typ_camel_ident,
-                )*>
-                    where $typ_camel_ident2: Fn($($fn_args,)*) -> $fn_ret_type + Copy,
+            ($typ2:ty, $typ_snake_ident2:ident) => {
+                impl $crate::fnlut::HasFn<($($fn_args,)*), $fn_ret_type> for $name
                 {
                     #[allow(refining_impl_trait_reachable)]
                     fn fn_type<T>(&self) -> impl Fn($($fn_args,)*) -> $fn_ret_type {
@@ -41,31 +44,25 @@ macro_rules! impl_fn_lut {
                 }
             };
         }
-        // impl<
-        //     $(
-        //         $typ_camel_ident: $trai,
-        //     )*
-        // > $name<$(
-        //     $typ_camel_ident,
-        // )*>
-        // where
-        //     $(
-        //         $typ_camel_ident: $trai,
-        //     )*
-        // {
-        //     pub fn new() -> Self {
-        //         Self {
-        //             $(
-        //                 $typ_snake_ident: <$typ_camel_ident as $trai>::$fn_name,
-        //             )*
-        //         }
-        //     }
-        // }
+        impl $name
+        where
+            $(
+                $typ: $trai,
+            )*
+        {
+            pub fn new() -> Self {
+                Self {
+                    $(
+                        $typ_snake_ident: <$typ as $trai>::$fn_name,
+                    )*
+                }
+            }
+        }
         $(
             mod $typ_snake_ident {
                 use super::*;
                 type This = $typ;
-                impl_for_one!($typ, $typ_camel_ident, $typ_snake_ident);
+                impl_for_one!($typ, $typ_snake_ident);
             }
         )*
     };
@@ -86,8 +83,8 @@ mod test {
         fn_ret_type This ;
 
         types
-        a / AArg = A,
-        b / BArg = B<A>
+        a = A,
+        b = B<A>
     );
 
     #[test]
@@ -95,10 +92,7 @@ mod test {
         use super::*;
         let a = A;
         let b = B(a.clone());
-        let clone_lut = CloneWitness {
-            a: A::clone,
-            b: <B<A> as Clone>::clone,
-        };
+        let clone_lut = CloneWitness::new();
         let a_clone = (clone_lut.fn_type::<(&A,)>())(&a);
         let b_clone = (clone_lut.fn_type::<(&B<A>,)>())(&b);
         // let clone_a_fn = <CloneWitness<A, B<A>> as HasFn<(&A,), A>>::fn_type(&clone_lut);
